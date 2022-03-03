0dcdb74114c02ec5a2aba6397b50efac92e7543e
Rajith Lahiru <rajithlahiru7@gmail.com>
2022-03-04 01:47:01 +0530

    first commit

---
+++ src/exct/Quizz.java
@@ -0,0 +1,11 @@
+package exct;
+
+public class Quizz {
+    String prompt;
+    String answer;
+
+    public Quizz(String prompt,String answer){
+        this.answer=answer;
+        this.prompt=prompt;
+    }
+}
